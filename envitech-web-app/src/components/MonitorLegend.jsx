78f945ac122478b2f58b3f4ed3f8bf36cbe8e7ab
YotamGit <yotam.gol@gmail.com>
2023-01-18 19:58:55 +0200

    add colors and labels to legend values

--- envitech-web-app/src/components/MonitorLegend.jsx
+++ envitech-web-app/src/components/MonitorLegend.jsx
@@ -6,7 +6,17 @@ const MonitorLegend = ({
       <div>{monitorName}</div>
       <div>
         {tags.map((tag) => (
-          <div key={tag.Label}>{JSON.stringify(tag)}</div>
+          <div key={tag.Label}>
+            <span
+              style={{
+                backgroundColor: tag.Color,
+                display: "inline-block",
+                height: "1rem",
+                width: "1rem",
+              }}
+            ></span>
+            <span>{tag.Label}</span>
+          </div>
         ))}
       </div>
       <button onClick={() => toggleLegend(false)}>close</button>
