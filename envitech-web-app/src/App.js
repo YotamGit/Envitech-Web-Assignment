e2131190319ad5114c06daa57f4e0848dd68cf6f
YotamGit <yotam.gol@gmail.com>
2023-01-18 18:10:43 +0200

    import data into the app

--- envitech-web-app/src/App.js
+++ envitech-web-app/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div></div>;
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    return await fetch("/Legends.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      const data = await getData();
+      setData(await data.json());
+    })();
+  }, []);
+
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default App;
