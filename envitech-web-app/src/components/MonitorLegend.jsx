1a2ff2846e13db0ee470d6b9ef3807d8f52e4eab
YotamGit <yotam.gol@gmail.com>
2023-01-18 23:06:38 +0200

    add styling to legend

--- envitech-web-app/src/components/MonitorLegend.jsx
+++ envitech-web-app/src/components/MonitorLegend.jsx
@@ -4,24 +4,33 @@ const MonitorLegend = ({
   legendProps: { monitorName, tags, toggleLegend },
 }) => {
   return (
-    <div className="monitor-legend">
-      <div>{monitorName}</div>
-      <div>
-        {tags.map((tag) => (
-          <div key={tag.Label}>
-            <span
-              style={{
-                backgroundColor: tag.Color,
-                display: "inline-block",
-                height: "1rem",
-                width: "1rem",
-              }}
-            ></span>
-            <span>{tag.Label}</span>
-          </div>
-        ))}
+    <div>
+      <div className="legend-background" onClick={() => toggleLegend(false)} />
+      <div className="monitor-legend">
+        <div className="legend-title">{monitorName}</div>
+        <div>
+          {tags.map((tag) => (
+            <div className="legend-property" key={tag.Label}>
+              <span
+                style={{
+                  backgroundColor: tag.Color,
+                  display: "inline-block",
+                  height: "2rem",
+                  width: "2rem",
+                }}
+              />
+
+              <span className="legend-label">{tag.Label}</span>
+            </div>
+          ))}
+        </div>
+        <button
+          className="close-legend-button"
+          onClick={() => toggleLegend(false)}
+        >
+          close
+        </button>
       </div>
-      <button onClick={() => toggleLegend(false)}>close</button>
     </div>
   );
 };
